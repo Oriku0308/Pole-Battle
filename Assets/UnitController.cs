d71b21384e0feebfd8b119ad7d5dc5912b872b1a
Oriku0308 <O.riku8863@gmail.com>
2025-09-05 00:55:44 +0900

    [add]一日目コミット -Unity環境設定：完了 -基本map作成：完了 -Character10体配置：完了 -NavMesh設定：完了 -基本移動テスト：完了

---
+++ Assets/UnitController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class UnitController : MonoBehaviour
+{
+    private NavMeshAgent agent;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+
+        // 基本設定
+        agent.speed = 5f;
+        agent.stoppingDistance = 0.1f;
+    }
+
+    // テスト用：クリックで移動
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out RaycastHit hit))
+            {
+                MoveTo(hit.point);
+            }
+        }
+    }
+
+    public void MoveTo(Vector3 position)
+    {
+        agent.SetDestination(position);
+    }
+}
